d010cbe71d813fff9c8f720ba3df0b352c020245
JITHENDRA-MOOLINTI <115027059+JITHENDRA-MOOLINTI@users.noreply.github.com>
2025-05-09 00:15:38 +0530

    Kadane's Algprithm

---
+++ Arrays/KadanesAlgorithm.java
@@ -0,0 +1,50 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class KadanesAlgorithm {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		int a[]=new int[size];
+		for(int i=0;i<size;i++) {
+			a[i]=sc.nextInt();
+		}
+		
+		//2 3 -8 7 -1 2 3
+		//Approach - 1 : Brute force
+		int maxSum=Integer.MIN_VALUE;
+		for(int i=0;i<a.length;i++) {
+			int currentSum=0;
+			for(int j=i;j<a.length;j++) {
+				currentSum+=a[j];
+			
+				if(currentSum>maxSum) {
+					maxSum=currentSum;
+					
+				}
+			}
+		}
+		
+		System.out.println(maxSum);
+		
+	
+		
+		//Approach 2 Kadane's Algorithm
+	     int currentSum=0;
+	     int maxSum=Integer.MIN_VALUE;
+	     for(int i=0;i<a.length;i++) {
+	    	 currentSum+=a[i];
+	    	 System.out.println(currentSum);
+	    	 maxSum=Math.max(currentSum, maxSum);
+	    	 System.out.println(maxSum);
+	    	 if(currentSum<0) {
+	    		 currentSum=0;
+	    	 }
+	    			 
+	     }
+	     System.out.println(maxSum);
+	}
+
+}
